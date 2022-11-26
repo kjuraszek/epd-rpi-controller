6d80cbe6909da0e82f75f997f4044e2f5c5d3bfa
kjuraszek <donkrissocorleone@gmail.com>
2022-11-26 15:11:54 +0100

    Update helpers.py

--- src/helpers.py
+++ src/helpers.py
@@ -9,8 +9,8 @@ from src.example import VIEWS
 
 def validate_config():
     assert not None in [KAFKA_VIEW_MANAGER_TOPIC, PRODUCER_INTERVAL]
-    assert type(KAFKA_VIEW_MANAGER_TOPIC) == str
-    assert type(PRODUCER_INTERVAL) == int and PRODUCER_INTERVAL >= 0
+    assert type(KAFKA_VIEW_MANAGER_TOPIC) is str
+    assert type(PRODUCER_INTERVAL) is int and PRODUCER_INTERVAL >= 0
 
 
 def validate_views():
