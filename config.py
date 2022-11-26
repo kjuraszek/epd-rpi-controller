6a5b2a82be2e7e0be1d2b9225a990b382fd58232
kjuraszek <donkrissocorleone@gmail.com>
2022-11-26 14:37:30 +0100

    Removed default config values

--- config.py
+++ config.py
@@ -3,5 +3,5 @@ import configparser
 cfg = configparser.ConfigParser()
 cfg.read('epd-rpi-controller.cfg')
 
-KAFKA_VIEW_MANAGER_TOPIC  = cfg['kafka'].get('view_manager_topic', 'epd_rpi_view_manager')
-PRODUCER_INTERVAL = cfg['main'].getint('producer_interval', 0)
+KAFKA_VIEW_MANAGER_TOPIC  = cfg['kafka'].get('view_manager_topic')
+PRODUCER_INTERVAL = cfg['main'].getint('producer_interval')
