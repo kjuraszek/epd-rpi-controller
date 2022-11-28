42e0292da871ccd0d2202f75299cc62403eed5ff
kjuraszek <donkrissocorleone@gmail.com>
2022-11-28 20:50:11 +0100

    Update config

--- config.py
+++ config.py
@@ -3,5 +3,8 @@ import configparser
 cfg = configparser.ConfigParser()
 cfg.read('epd-rpi-controller.cfg')
 
-KAFKA_VIEW_MANAGER_TOPIC  = cfg['kafka'].get('view_manager_topic')
 PRODUCER_INTERVAL = cfg['main'].getint('producer_interval')
+USE_MOCKED_EPD = cfg['main'].getboolean('use_mocked_epd')
+MOCKED_EPD_WIDTH = cfg['main'].getint('mocked_epd_width')
+MOCKED_EPD_HEIGHT = cfg['main'].getint('mocked_epd_height')
+KAFKA_VIEW_MANAGER_TOPIC  = cfg['kafka'].get('view_manager_topic')
