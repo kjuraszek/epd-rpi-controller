5d53a9b55c7e5be441fb386a847242ea3d63327a
kjuraszek <donkrissocorleone@gmail.com>
2022-12-04 15:29:20 +0100

    Clear EPD on exit as option
    - updated Clear method in `MockedEPD` class
    - added `clear_epd_on_exit` option to config
    - adjusted controller and validation to use this new option

--- controller/src/helpers.py
+++ controller/src/helpers.py
@@ -2,16 +2,17 @@
 Module contains helper functions
 '''
 
-from config import KAFKA_VIEW_MANAGER_TOPIC, PRODUCER_INTERVAL, USE_MOCKED_EPD, MOCKED_EPD_WIDTH, MOCKED_EPD_HEIGHT
+from config import KAFKA_VIEW_MANAGER_TOPIC, PRODUCER_INTERVAL, USE_MOCKED_EPD, MOCKED_EPD_WIDTH, MOCKED_EPD_HEIGHT, CLEAR_EPD_ON_EXIT
 from src import View
 from src.example import VIEWS
 
 
 def validate_config():
-    assert not None in [KAFKA_VIEW_MANAGER_TOPIC, PRODUCER_INTERVAL, USE_MOCKED_EPD]
+    assert not None in [KAFKA_VIEW_MANAGER_TOPIC, PRODUCER_INTERVAL, USE_MOCKED_EPD, CLEAR_EPD_ON_EXIT]
     assert type(KAFKA_VIEW_MANAGER_TOPIC) is str
     assert type(PRODUCER_INTERVAL) is int and PRODUCER_INTERVAL >= 0
     assert type(USE_MOCKED_EPD) is bool
+    assert type(CLEAR_EPD_ON_EXIT) is bool
     if USE_MOCKED_EPD:
         assert type(MOCKED_EPD_WIDTH) is int and MOCKED_EPD_WIDTH > 0
         assert type(MOCKED_EPD_HEIGHT) is int and MOCKED_EPD_HEIGHT > 0
