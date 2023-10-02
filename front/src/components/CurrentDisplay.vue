406837c7743cad66ef37ea48834770e2f728769e
kjuraszek <donkrissocorleone@gmail.com>
2023-10-02 20:24:30 +0200

    Switch view with a swipe

--- front/src/components/CurrentDisplay.vue
+++ front/src/components/CurrentDisplay.vue
@@ -1,20 +1,27 @@
 <template>
-  <v-img
-    v-if="currentImage"
-    :class="currentImageClass" 
-    :src="currentImage"
-  />
-  <p v-else>
-    Unable to load an image, check if API and EPD are working.
-  </p>
-  <CurrentDisplayInfo />
+  <div>
+    <v-img
+      v-if="currentImage"
+      v-touch="{
+        left: () => swipeHandler('prev'),
+        right: () => swipeHandler('next'),
+      }"
+      :class="currentImageClass" 
+      :src="currentImage"
+    />
+    <p v-else>
+      Unable to load an image, check if API and EPD are working.
+    </p>
+    <CurrentDisplayInfo />
+  </div>
 </template>
 
 <script>
   import CurrentDisplayInfo from '@/components/CurrentDisplayInfo.vue'
   import { useEpdStatusStore } from '@/stores/epdStatus'
   import { useUiStatusStore } from '@/stores/uiStatus'
-  import { mapState } from 'pinia'
+  import { mapWritableState, mapState, mapActions } from 'pinia'
+  import { API } from '@/consts'
 
   export default {
     components: {
@@ -22,10 +29,37 @@
     },
     computed: {
       ...mapState(useEpdStatusStore, ['currentImage']),
-      ...mapState(useUiStatusStore, ['loadingImage']),
+      ...mapWritableState(useUiStatusStore, ['successAlert', 'warningAlert', 'errorAlert', 'loadingImage']),
+      
       currentImageClass () {
         return this.loadingImage ? 'current-image img-loading-shadow' : 'current-image img-no-shadow'
       }
+    },
+    methods: {
+      swipeHandler(direction) {
+        fetch(`${API}/${direction}`)
+        .then(response => {
+          return response
+        })
+        .then(data => {
+           if (data.status >= 500) {
+            this.resetAlerts()
+            this.errorAlert = true
+           } else if (data.status >= 400) {
+            this.resetAlerts()
+            this.warningAlert = true
+           } else {
+            this.resetAlerts()
+            this.successAlert = true
+           }
+        })
+        .catch(error => {
+          this.resetAlerts()
+          this.errorAlert = true
+          console.error('Error while switching a view:', error);
+        })
+      },
+      ...mapActions(useUiStatusStore, ['resetAlerts']),
     },
   }
 </script>
